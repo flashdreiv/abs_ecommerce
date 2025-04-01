d38beef4aa05701cfa5be345590a0802b6aec672
Dreivan Orprecio <drei@Dreivans-MacBook-Air.local>
2025-04-01 11:59:18 +0800

    Configure allowed hosts

--- abs_ecommerce/settings.py
+++ abs_ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'oe=)4*p)_-1*o5#0i6pzk&le1o^18##_qve83nz^c6%7ktgk=j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
